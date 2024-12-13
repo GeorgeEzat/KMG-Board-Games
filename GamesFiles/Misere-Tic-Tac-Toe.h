f0fbe825c1a263df561ee613a13944177c8dbc16
GeorgeEzat <163030821+GeorgeEzat@users.noreply.github.com>
2024-12-13 18:02:38 +0200

    Revert "Merge branch 'main' of https://github.com/GeorgeEzat/KMG-Board-Games"
    This reverts commit 0f42f44aea73e8b904920da88f12932a9544c9e7, reversing
    changes made to b0ff8995096bdb0937b17d509a7fc6433e4ca166.

--- GamesFiles/Misere-Tic-Tac-Toe.h
+++ GamesFiles/Misere-Tic-Tac-Toe.h
@@ -5,126 +5,124 @@
 #include <iostream>
 #include <iomanip>
 #include <cctype>  // for toupper()
-#include <cstdlib> // for rand()
-using namespace std;
-
-int move_tracker = 0; // Tracks the state of the game
-
+int num = 0;
 template <typename T>
-class Misere_Board : public Board<T> {
+class Misere_Board:public Board<T> {
 public:
-    // Constructor initializes a 3x3 board with empty cells
-    Misere_Board() {
-        this->rows = this->columns = 3;
-        this->board = new char*[this->rows];
-        for (int row_idx = 0; row_idx < this->rows; row_idx++) {
-            this->board[row_idx] = new char[this->columns];
-            for (int col_idx = 0; col_idx < this->columns; col_idx++) {
-                this->board[row_idx][col_idx] = 0; // Empty cell
-            }
+    Misere_Board(){
+    this->rows = this->columns = 3;
+    this->board = new char*[this->rows];
+    for (int i = 0; i < this->rows; i++) {
+        this->board[i] = new char[this->columns];
+        for (int j = 0; j < this->columns; j++) {
+            this->board[i][j] = 0;
         }
-        this->n_moves = 0; // Initialize move count to 0
+    }
+    this->n_moves = 0;
+    }
+    bool update_board (int x, int y, T symbol){
+            // Only update if move is valid
+    if (num > 0){
+        return true;
+    }
+    if (!(x < 0 || x >= this->rows || y < 0 || y >= this->columns) && (this->board[x][y] == 0|| symbol == 0)) {
+        if (symbol == 0){
+            this->n_moves--;
+            this->board[x][y] = 0;
+        }
+        else {
+            this->n_moves++;
+            this->board[x][y] = toupper(symbol);
+        }
+
+        return true;
+    }
+    return false;
+    }
+    void display_board (){
+            for (int i = 0; i < this->rows; i++) {
+        cout << "\n| ";
+        for (int j = 0; j < this->columns; j++) {
+            cout << "(" << i << "," << j << ")";
+            cout << setw(2) << this->board[i][j] << " |";
+        }
+        cout << "\n--------------------------";
+    }
+    cout << endl;
     }
 
-    // Updates the board with a player's move, if valid
-    bool update_board(int row, int col, T symbol) {
-        if (move_tracker > 0) {
-            return true; // Skip update if the game is over
+    bool is_win(){
+    if (num > 0){
+        return true;
+    }
+    // Check rows and columns
+    for (int i = 0; i < this->rows; i++) {
+        if ((this->board[i][0] == this->board[i][1] && this->board[i][1] == this->board[i][2] && this->board[i][0] != 0) ||
+            (this->board[0][i] == this->board[1][i] && this->board[1][i] == this->board[2][i] && this->board[0][i] != 0)) {
+            num = this->n_moves+3;
         }
-        if (!(row < 0 || row >= this->rows || col < 0 || col >= this->columns) && (this->board[row][col] == 0 || symbol == 0)) {
-            if (symbol == 0) {
-                this->n_moves--;
-                this->board[row][col] = 0; // Reset cell
-            } else {
-                this->n_moves++;
-                this->board[row][col] = toupper(symbol); // Update cell with symbol
-            }
-            return true;
-        }
-        return false; // Invalid move
     }
 
-    // Displays the current state of the board
-    void display_board() {
-        for (int row_idx = 0; row_idx < this->rows; row_idx++) {
-            cout << "\n| ";
-            for (int col_idx = 0; col_idx < this->columns; col_idx++) {
-                cout << "(" << row_idx << "," << col_idx << ")";
-                cout << setw(2) << this->board[row_idx][col_idx] << " |";
-            }
-            cout << "\n--------------------------";
-        }
-        cout << endl;
+    // Check diagonals
+    if ((this->board[0][0] == this->board[1][1] && this->board[1][1] == this->board[2][2] && this->board[0][0] != 0) ||
+        (this->board[0][2] == this->board[1][1] && this->board[1][1] == this->board[2][0] && this->board[0][2] != 0)) {
+        num= this->n_moves+3;
     }
 
-    // Checks if the game has been won
-    bool is_win() {
-        if (move_tracker > 0) {
-            return true; // Game already won
-        }
-        // Check rows and columns for a win
-        for (int row_idx = 0; row_idx < this->rows; row_idx++) {
-            if ((this->board[row_idx][0] == this->board[row_idx][1] && this->board[row_idx][1] == this->board[row_idx][2] && this->board[row_idx][0] != 0) ||
-                (this->board[0][row_idx] == this->board[1][row_idx] && this->board[1][row_idx] == this->board[2][row_idx] && this->board[0][row_idx] != 0)) {
-                move_tracker = this->n_moves + 3;
-            }
-        }
-
-        // Check diagonals for a win
-        if ((this->board[0][0] == this->board[1][1] && this->board[1][1] == this->board[2][2] && this->board[0][0] != 0) ||
-            (this->board[0][2] == this->board[1][1] && this->board[1][1] == this->board[2][0] && this->board[0][2] != 0)) {
-            move_tracker = this->n_moves + 3;
-        }
-
-        return false; // No win detected
+    return false;
     }
 
-    // Checks if the game is a draw
     bool is_draw() override {
         return (this->n_moves == 9 && !is_win());
     }
-
-    // Determines if the game is over
     bool game_is_over() override {
-        if (move_tracker != 0) {
-            move_tracker = 0; // Reset tracker for the next game
+        if (num != 0){
+            num = 0;
         }
         return (is_win() || is_draw());
     }
 };
 
+
 template <typename T>
 class MiserePlayer : public Player<T> {
 public:
-    // Constructor initializes player with name and symbol
-    MiserePlayer(string player_name, T player_symbol) : Player<T>(player_name, player_symbol) {}
-
-    // Prompts player to input their move
-    void getmove(int& row, int& col) {
-        if (move_tracker > 0) {
-            return; // Skip input if the game is over
-        } else {
-            cout << "\nPlease enter your move (row, col) separated by spaces: ";
-            cin >> row >> col;
+    MiserePlayer (string name, T symbol) : Player<T>(name, symbol) {} 
+    void getmove(int& x, int& y) {
+        if (num > 0){
+            return; //will not take input 
         }
+        else{
+    cout << "\nPlease enter your move x and y (0 to 2) separated by spaces: ";
+    cin >> x >> y;
+        }
+
     }
+
 };
 
 template <typename T>
-class MisereRandom_Player : public RandomPlayer<T> {
+class MisereRandom_Player : public RandomPlayer<T>{
 public:
-    // Constructor initializes a random computer player
-    MisereRandom_Player(T player_symbol) : RandomPlayer<T>(player_symbol) {
-        this->dimension = 3;
-        this->name = "Random Computer Player";
-        srand(static_cast<unsigned int>(time(0))); // Seed random number generator
-    }
-
-    // Generates a random move for the player
-    void getmove(int& row, int& col) {
-        row = rand() % this->dimension; // Random row between 0 and 2
-        col = rand() % this->dimension; // Random column between 0 and 2
+    MisereRandom_Player (T symbol): RandomPlayer<T>(symbol) {
+    this->dimension = 3;
+    this->name = "Random Computer Player";
+    srand(static_cast<unsigned int>(time(0)));  // Seed the random number generator
+}
+    void getmove(int &x, int &y) {
+    x = rand() % this->dimension;  // Random number between 0 and 2
+    y = rand() % this->dimension;
     }
 };
+
+
+
+
+
+
+
+
+
+
 
 #endif
