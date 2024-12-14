8a87ebae8256bbb9ac1eb608166099f43cc3832e
Kerolus Akram <162516373+Kerolusss@users.noreply.github.com>
2024-12-14 08:30:49 +0200

    Add random player

--- GamesFiles/Numerical-Tic-Tac-Toe.h
+++ GamesFiles/Numerical-Tic-Tac-Toe.h
@@ -7,7 +7,7 @@ template <typename T>
 class NUM_X_O_Board: public Board<T> {
 public:
     NUM_X_O_Board ();
-    bool update_board (int x , int y , T symbol);
+    bool update_board (int x, int y, T symbol);
     void display_board () ;
     bool is_win() ;
     bool is_draw();
@@ -18,23 +18,22 @@ public:
 template <typename T>
 class NUM_X_O_Player : public Player<T> {
 public:
-    NUM_X_O_Player (string name, T symbol,bool is_0dd, vector<int> &available_numbers);
+    NUM_X_O_Player (string name, T symbol, vector<int> &available_numbers);
     bool hasNumber(int number);
-
     void removeNumber(int number);
-
     void getmove(int& x, int& y) override ;
 private:
-    bool odd;
     vector<int> num;
 
 };
 
 template <typename T>
-class NUM_X_O_Random_Player : public RandomPlayer<T>{
+class NUM_X_O_Random_Player : public RandomPlayer<T> {
 public:
-    NUM_X_O_Random_Player (T symbol,bool is_0dd, vector<int> &available_numbers);
+    NUM_X_O_Random_Player (T symbol, vector<int> &available_numbers);
     void getmove(int &x, int &y) ;
+    vector<int> Rand_num;
+
 };
 
 
@@ -67,7 +66,7 @@ template <typename T>
 bool NUM_X_O_Board<T>::update_board(int x, int y, T mark) {
     // Only update if move is valid
     if (!(x < 0 || x >= this->rows || y < 0 || y >= this->columns) && (this->board[x][y] == 0|| mark == 0)  && mark != -1) {
-        if (mark == 0){
+        if (mark == 0) {
             this->n_moves--;
             this->board[x][y] = 0;
         }
@@ -101,14 +100,14 @@ bool NUM_X_O_Board<T>::is_win() {
     // Check rows and columns
     for (int i = 0; i < this->rows; i++) {
         if (((this->board[i][0] + this->board[i][1] + this->board[i][2] )== 15 && this->board[i][0] != 0 &&  this->board[i][1] != 0 &&  this->board[i][2] != 0) ||
-                ((this->board[0][i] + this->board[1][i] + this->board[2][i] )== 15 && this->board[0][i] != 0 &&  this->board[1][i] != 0 &&  this->board[2][i] != 0)) {
+            ((this->board[0][i] + this->board[1][i] + this->board[2][i] )== 15 && this->board[0][i] != 0 &&  this->board[1][i] != 0 &&  this->board[2][i] != 0)) {
             return true;
         }
     }
 
     // Check diagonals
     if (((this->board[0][0] + this->board[1][1] + this->board[2][2] )==15 && this->board[2][2] !=0 && this->board[0][0] != 0&& this->board[1][1] != 0) ||
-            ((this->board[0][2] + this->board[1][1] + this->board[2][0]) ==15 && this->board[0][2] != 0 && this->board[1][1] != 0 && this->board[2][0] != 0)) {
+        ((this->board[0][2] + this->board[1][1] + this->board[2][0]) ==15 && this->board[0][2] != 0 && this->board[1][1] != 0 && this->board[2][0] != 0)) {
         return true;
     }
 
@@ -130,27 +129,19 @@ bool NUM_X_O_Board<T>::game_is_over() {
 
 // Constructor for NUM_X_O_Player
 template <typename T>
-NUM_X_O_Player<T>::NUM_X_O_Player(string name, T symbol,bool is_0dd, vector<int> &available_numbers) : Player<T>(name, symbol) {
-    odd = is_0dd;
+NUM_X_O_Player<T>::NUM_X_O_Player(string name, T symbol, vector<int> &available_numbers) : Player<T>(name, symbol) {
     num = available_numbers;
 
 }
 
-//template <typename T>
-//void NUM_X_O_Player<T>::getmove(int& x, int& y) {
-//    cout << "\nPlease enter your move x and y (0 to 2) and value separated by spaces: ";
-//    cin >> x >> y ; // INPUT MARK HERE
-//    // CHEK IF THE MARK IS VALID USING HASNUMBER FUN IF YES REMOVE IT FROM VECTOR USING REMOVENUMBER FUNCTION
-//    // if not vaild set the mark -1
-//
-//}
+
 
 template <typename T>
 void NUM_X_O_Player<T>::getmove(int& x, int& y) {
     while (true) {
         int mark; // To hold the value for the move
 
-        cout << "\nPlease enter your move (x y value) and value separated by spaces: ";
+        cout << "\nPlease enter your move (x y value) and value separated by spaces (0 0 9): ";
         cin >> x >> y >> mark;
 
         // Check if the mark is valid using hasNumber function
@@ -160,8 +151,7 @@ void NUM_X_O_Player<T>::getmove(int& x, int& y) {
             break;
 
         } else {
-
-            this->symbol = -1;
+            this->symbol = -1; // will not update the board
             break;
         }
     }
@@ -176,17 +166,15 @@ template <typename T>
 bool NUM_X_O_Player<T>::hasNumber(int number) {
     if (std::find(num.begin(), num.end(), number) != num.end()) {
         return true;
-
     }
-
     return false;
 
 }
 
 // Constructor for NUM_X_O_Random_Player
 template <typename T>
-NUM_X_O_Random_Player<T>::NUM_X_O_Random_Player (T symbol,bool is_0dd, vector<int> &available_numbers) : RandomPlayer<T>(symbol) {
-
+NUM_X_O_Random_Player<T>::NUM_X_O_Random_Player (T symbol, vector<int> &available_numbers) : RandomPlayer<T>(symbol) {
+    Rand_num = available_numbers;
     this->dimension = 3;
     this->name = "Random Computer Player";
     srand(static_cast<unsigned int>(time(0)));  // Seed the random number generator
@@ -194,8 +182,10 @@ NUM_X_O_Random_Player<T>::NUM_X_O_Random_Player (T symbol,bool is_0dd, vector<in
 
 template <typename T>
 void NUM_X_O_Random_Player<T>::getmove(int& x, int& y) {
-    x = rand() % this->dimension;  // Random number between 0 and 2
+    x = rand() % this->dimension;
     y = rand() % this->dimension;
+
+    this->symbol = Rand_num[rand() % Rand_num.size()];
 }
 
 
@@ -205,4 +195,3 @@ void NUM_X_O_Random_Player<T>::getmove(int& x, int& y) {
 
 
 #endif //NUM_X_O_H
-
